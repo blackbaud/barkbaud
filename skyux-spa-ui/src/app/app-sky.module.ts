690c9ff63ca2ed0408fa87dc5759a3c3ec521b9c
Val Kuppuswamy <val.kuppuswamy@blackbaud.com>
2025-01-29 15:15:35 -0600

    Upgrade to SKY UX 11

--- skyux-spa-ui/src/app/app-sky.module.ts
+++ skyux-spa-ui/src/app/app-sky.module.ts
@@ -21,9 +21,10 @@ import {
   SkyCheckboxModule
 } from '@skyux/forms';
 import { SkyI18nModule } from '@skyux/i18n';
+import { SkyIconModule } from '@skyux/icon';
 import {
   SkyAlertModule,
-  SkyIconModule,
+  
   SkyKeyInfoModule,
   SkyLabelModule,
   SkyTokensModule,
